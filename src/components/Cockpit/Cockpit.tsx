613a9c86b663c64daa531e7ee4e3227068ca7f74
pradeecs73 <pradeecs73@gmail.com>
2021-05-02 12:23:25 +0530

    hook commit

--- src/components/Cockpit/Cockpit.tsx
+++ src/components/Cockpit/Cockpit.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component,PureComponent } from 'react';
 
 import  './Cockpit.css';
 import AuthContext from './../../context/auth-context';
